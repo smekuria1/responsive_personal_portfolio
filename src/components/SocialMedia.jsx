235b0f0c8a10e6b321ecbd83c7e6925e21ba7b1c
smekuria1 <smekuria@matignon.org>
2022-06-22 18:06:33 -0400

    removed empty social media icons-remember to connect twitter after deployment

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { BsTwitter } from 'react-icons/bs';
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
       <div>
         <BsTwitter />
-      </div>
-      <div>
-        <FaFacebookF />
-      </div>
-      <div>
-        <BsInstagram />
       </div>
     </div>
   );
